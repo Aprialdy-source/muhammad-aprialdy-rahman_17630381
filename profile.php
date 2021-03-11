ded2126dd23faf88cb0d57dcd4c0d7e33ae28982
Aprialdy-source <rknprojext09@gmail.com>
2021-03-11 16:01:25 +0800

    commit pertama : project praktikum Muhammad Aprialdy Rahman - 17630381

---
+++ profile.php
@@ -0,0 +1,58 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/aw.png" alt="Muhammad Aprialdy Rahman" width="160" class="rounded-pill">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM:</strong></td>
+                        <td align="left" width="50%">17630381</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Aprialdy Rahman</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 24 April 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0896 2728 1231</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">rknprojext09@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.facebook.com/Aprialdy.Rahman/" target="_blank"
+                                style="text-decoration:none;">
+                                <img src="svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/apri_aldy/" target="_blank"
+                                style="text-decoration:none;">
+                                <img src="svg/029-instagram.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.youtube.com/channel/UCAtUYUY_EXnZSn6o7Gqk7og" target="_blank"
+                                style="text-decoration:none;">
+                                <img src="svg/001-youtube.svg" width="30" alt="facebook">
+                            </a>
+
+
+
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php" ?>
